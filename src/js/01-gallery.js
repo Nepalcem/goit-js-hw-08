0823e353563c2c6f81fc01abf62c8d89dee06f6e
Nepalcem <mark.emailtest@gmail.com>
2023-03-26 15:36:17 +0300

    task-1 update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryMarkup = document.querySelector(".gallery");
+
+const galleryList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`;
+  })
+  .join("");
+
+galleryMarkup.insertAdjacentHTML("afterbegin", galleryList);
+
+const lightBox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
