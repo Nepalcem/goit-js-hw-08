fc29fc1ba6003fdfc9ea14166553b0e86ec067cd
Nepalcem <mark.emailtest@gmail.com>
2023-03-26 23:41:07 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const FORM_STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('input[name="email"]');
+const message = form.querySelector('textarea[name="message"]');
+
+recoverMessageFromStorage();
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  localStorage.removeItem(FORM_STORAGE_KEY);
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(formData);
+  e.currentTarget.reset();
+}
+function onFormInput(e) {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  const textInput = JSON.stringify(formData);
+
+  localStorage.setItem(FORM_STORAGE_KEY, textInput);
+}
+
+function recoverMessageFromStorage() {
+  const savedData = localStorage.getItem(FORM_STORAGE_KEY);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    email.value = formData.email;
+    message.value = formData.message;
+  } else {
+    email.value = '';
+    message.value = '';
+  }
+}
